d972927bc220d85f184fc52ef246af4188cca1e2
cristhoferChow <viros123456789@gmail.com>
2024-11-23 04:27:38 -0300

    apagar para atualização

--- src/main/resources/schema-postgresql.sql
+++
@@ -1,9 +0,0 @@
-create table IF NOT EXISTS livro(
-id serial PRIMARY KEY,
-nome VARCHAR(45),
-ano INT,
-autor VARCHAR(30),
-editora VARCHAR(30),
-
- CONSTRAINT chk_ano CHECK (ano >= 1900 AND ano <= EXTRACT(YEAR FROM CURRENT_DATE))
-);
